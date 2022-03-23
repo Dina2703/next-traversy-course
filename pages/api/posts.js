5d3dd8ed8c68f1a2ea66b30685892192d0f3015d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-23 11:33:47 -0400

    fetching data from mongodb

---
+++ pages/api/posts.js
@@ -0,0 +1,18 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("Cluster0");
+
+  switch (req.method) {
+    case "POST":
+      let bodyObject = JSON.parse(req.body);
+      let newPost = await db.collection("blogs").insertOne(bodyObject);
+      res.json(newPost.ops[0]);
+      break;
+    case "GET":
+      const posts = await db.collection("blogs").find({}).toArray();
+      res.json({ status: 200, data: blogs });
+      break;
+  }
+}
